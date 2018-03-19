b0ab1152c16fd67131b86ba46b7d7d3aaff1f272
Barrett Otte <barrettotte@gmail.com>
2018-03-19 15:41:58 -0400

    Implemented TheMovieDB API

--- server.js
+++ server.js
@@ -24,10 +24,13 @@
             
             CREATE TABLE dvdlist(
             ID int(11) NOT NULL auto_increment,
-            title varchar(25) NOT NULL,
+            title varchar(50) NOT NULL,
             description text NOT NULL,
             quantity int(11) NOT NULL,
             image_url text NOT NULL,
+            TMDB_ID int(11) NOT NULL
+            release_date date NOT NULL,
+            rating varchar(5) NOT NULL,
             PRIMARY KEY (ID)
             );
 */
@@ -53,6 +56,18 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.static(__dirname + 'public'));  
+
+
+//Assign site constants
+const portNumber = "3000";
+const siteTitle = "CRUD APP";
+const baseURL = "http://localhost:" + portNumber + "/";
+const TMDB_API_KEY = "244a059cc1db5224bab95119b674815b"; //Oh no don't steal my api key!! D:
+const TMDB_BasePoster = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+
+//Use TheMovieDB API to pull information about movies
+const TheMovieDB = require('moviedb')(TMDB_API_KEY);
 
 
 
@@ -65,10 +80,11 @@ const connection = mysql.createConnection({
 });
 
 
-//Assign site constants
-const portNumber = "3000";
-const siteTitle = "CRUD APP";
-const baseURL = "http://localhost:" + portNumber + "/";
+
+// Connect to Server
+var server = app.listen(portNumber, function(){
+	console.log("Server listening on 127.0.0.1:" + portNumber + " ...");
+});
 
 
 
@@ -84,31 +100,93 @@ app.get('/', function (req, res){
 });
 
 
-//Display form to add dvd entry
+
+//Display form to Search for DVD Entry to Add
 app.get('/dvd/add', function (req, res){
-    res.render('pages/add-dvd.ejs', {
+    res.render('pages/add-dvd-search.ejs', {
         siteTitle : siteTitle,
-        pageTitle : "Add new DVD",
-        items : ''
+        pageTitle : "Search new DVD",
+        items : null,
+        isSearch : true,
     });
 });
 
 
-//Add entry to dvd database
-app.post('/dvd/add', function(req, res){
-    var query = "INSERT INTO dvdlist (title, description, quantity, image_url)";
-    query += " VALUES ("
-    query += " '" + req.body.title + "',";
-    query += " '" + req.body.description + "',";
-    query += " " + req.body.quantity + ",";
-    query += " '" + req.body.image_url + "')";
-    
-    console.log("Query from /dvd/add  :\n" + query);
-    
-    connection.query(query, function(err, result){
-        if(err) throw err;
-        res.redirect(baseURL);
+
+//Display form for quantity of selected DVD
+app.get('/dvd/add/:TMDB_ID', function(request, response){
+    TheMovieDB.movieInfo({id: request.params.TMDB_ID}, function(err, res){
+        response.render('pages/add-dvd.ejs', {
+            siteTitle : siteTitle,
+            pageTitle : "Add Selected Movie",
+            isSearch : false,
+            movieData : res
+        });
     });
+});
+
+
+
+//Add selected DVD with specified quantity to DB
+app.post('/dvd/add/:TMDB_ID', function(request, response){
+    
+    var id = (""+request.params.TMDB_ID).substring(1, request.params.TMDB_ID.length);
+    
+    TheMovieDB.movieInfo({id: id}, function(err, res){
+        
+        var desc = (res.overview.replace("'", " ")).replace('"', " ");
+        
+        var query = "INSERT INTO dvdlist (title, description, quantity, " +
+                                            "image_url, TMDB_ID, release_date, rating)";
+        query += " VALUES ("
+            query += " '"   + res.title + "',";
+            query += " '"   + desc + "',";
+            query += " "    + request.body.quantity + ",";
+            query += " '"   + TMDB_BasePoster + res.poster_path + "',";
+            query += " "    + id + ",";
+            query += " '"   + res.release_date + "',";
+            query += " "    + res.vote_average;
+        query += ")";
+        
+        console.log("[ADDING ENTRY] Query  :\n" + query);
+        
+        connection.query(query, function(err, result){
+            if(err) throw err;
+            response.redirect(baseURL);
+        });
+    });
+});
+
+
+
+//Search for entries of DVDs from TMDB
+app.post('/dvd/add', function(request, response){
+    
+    /*Add logic for looking through already available DVDs here...
+        That way we can just add quantity instead of searching again
+        for something we already have.
+    */
+    /*NOTE: Due to limited queries/second from TMDB's API,
+        this will only show the first 20 entries. Implementing pages
+        would be a workaround for this.
+    */
+    
+    TheMovieDB.searchMovie({query: request.body.title }, function(err, res){
+        if(res != null && res.total_results >= 1){
+            //console.log(res.results);
+            console.log("[ADDING ENTRY] Found " + res.total_results + " results.");
+            console.log("[ADDING ENTRY] Loading search results...");
+            response.render('pages/add-dvd-search.ejs', {
+                siteTitle : siteTitle,
+                pageTitle : "Select DVD",
+                items : res.results,
+            });
+        }
+        else{
+            console.log("[ADDING ENTRY] No search results found.");
+            response.redirect(baseURL + 'dvd/add');
+        }
+    }); 
 });
 
 
@@ -125,16 +203,19 @@ app.get('/dvd/edit/:dvdID', function(req, res){
 });
 
 
+
 //Update dvd entry with edited data
 app.post('/dvd/edit/:dvdID', function(req, res){
     var query = "UPDATE dvdlist SET";
     query += " title = '" + req.body.title + "',";
-    query += " description = '" + req.body.description + "',";
+    query += " release_date = '" + req.body.release_date + "',"
     query += " image_url = '" + req.body.image_url + "',";
+    query += " description = '" + req.body.description + "',";
+    query += " rating = " + req.body.rating + ",";
     query += " quantity = " + req.body.quantity + "";
     query += " WHERE dvdlist . ID = " + req.body.ID + "";
     
-    console.log("Query from /dvd/edit  :\n" + query);
+    console.log("[EDITING ENTRY] Query :\n" + query);
     
     connection.query(query, function(err, result){
         if(err) throw err;
@@ -145,9 +226,12 @@ app.post('/dvd/edit/:dvdID', function(req, res){
 });
 
 
+
 //Delete dvd entry from database
 app.get('/dvd/delete/:dvdID', function(req, res){
-    console.log("Deleting entry " + req.params.dvdID);
+    
+    console.log("[DELETING ENTRY] Deleted Item " + req.params.dvdID);
+    
     connection.query("DELETE FROM dvdlist WHERE ID='" + req.params.dvdID + "'", function(err, result){
         if(err) throw err;
         if(result.affectedRows){
@@ -156,14 +240,6 @@ app.get('/dvd/delete/:dvdID', function(req, res){
     });
 });
 
-
-
-
-
-// Connect to Server
-var server = app.listen(portNumber, function(){
-	console.log("Server listening on 127.0.0.1:" + portNumber + " ...");
-});
 
 
 
